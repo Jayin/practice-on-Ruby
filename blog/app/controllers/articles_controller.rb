f2b22d1785d0152947b0008235b0326bd6f15548
Jayin <tonjayin@gmail.com>
2015-01-08 16:15:58 +0800

    End 5

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,16 +1,20 @@
 class ArticlesController < ApplicationController
+
     def index
         @articles = Article.all
     end
 
     def new
+        @article = Article.new
     end
 
     def create
         @article = Article.new(article_params)
-
-        @article.save
-        redirect_to @article
+        if @article.save 
+            redirect_to @article
+        else 
+            render 'new'
+        end
         # render plain: params[:article].inspect
     end
 
@@ -18,6 +22,26 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+     
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+     
+      redirect_to articles_path
+    end
 private
     def article_params
         params.require(:article).permit(:title,:text)
